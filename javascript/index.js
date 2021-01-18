82a83c13ea738fb527c5db7bf750f00c697b5be4
Mavis <llingchen.27@gmail.com>
2021-01-18 16:32:38 +0800

    preactice leetcode

--- javascript/index.js
+++ javascript/index.js
@@ -36,3 +36,60 @@
 // }
 // num(10);
 
+// /**
+//  * @param {number[]} A
+//  * @return {number[]}
+//  */
+// var sortArrayByParity = function (A) {
+//     if (A.length < 2) {
+//         return A;
+//     }
+
+//     let temp = [];
+//     A.forEach(element => {
+//         element % 2 == 0 ? temp.unshift(element) : temp.push(element);
+//     });
+//     return temp;
+// };
+
+// $a = [3, 1, 2, 4];
+// sortArrayByParity($a);
+// console.log($a);
+
+// /**
+//  * @param {number[]} nums
+//  * @return {number[]}
+//  */
+// var runningSum = function (nums) {
+
+//     for (let i = 1; i < nums.length; i++) {
+//         nums[i] += nums[i - 1];
+//     }
+//     return nums;
+// };
+// let nums = [1, 2, 3, 4];
+// console.log(runningSum(nums));
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayPairSum = function (nums) {
+
+    let result = 0;
+    let numsSort = nums.sort((a, b) => a - b); // sorting from min to big
+    for (let i = 0; i < nums.length; i += 2) {
+        result += Math.min(numsSort[i], numsSort[i + 1]); // += =>> ++
+    }
+    return result;
+
+};
+let nums = [1, 4, 3, 2];
+// Output: 4
+// Explanation: All possible pairings (ignoring the ordering of elements) are:
+// 1. (1, 4), (2, 3) -> min(1, 4) + min(2, 3) = 1 + 2 = 3
+// 2. (1, 3), (2, 4) -> min(1, 3) + min(2, 4) = 1 + 2 = 3
+// 3. (1, 2), (3, 4) -> min(1, 2) + min(3, 4) = 1 + 3 = 4
+// So the maximum possible sum is 4.
+// 兩個一組，兩組比對，取最小的值互加
+console.log(arrayPairSum(nums));
