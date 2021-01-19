bec54cd1c5c523075f751eb05c743a9a435f14f2
Mavis <llingchen.27@gmail.com>
2021-01-20 07:32:17 +0800

    add leetcode solution problems into new folder array

---
+++ javascript/Array/1431.js
@@ -0,0 +1,44 @@
+/*
+Kids With the Greatest Number of Candies
+
+Given the array candies and the integer extraCandies, where candies[i] represents the number of candies that the ith kid has.
+
+For each kid check if there is a way to distribute extraCandies among the kids such that he or she can have the greatest number of candies among them. Notice that multiple kids can have the greatest number of candies.
+
+--------------------------------------------------
+EG.
+Input: candies = [2,3,5,1,3], extraCandies = 3
+Output: [true,true,true,false,true] 
+Explanation: 
+Kid 1 has 2 candies and if he or she receives all extra candies (3) will have 5 candies --- the greatest number of candies among the kids. 
+Kid 2 has 3 candies and if he or she receives at least 2 extra candies will have the greatest number of candies among the kids. 
+Kid 3 has 5 candies and this is already the greatest number of candies among the kids. 
+Kid 4 has 1 candy and even if he or she receives all extra candies will only have 4 candies. 
+Kid 5 has 3 candies and if he or she receives at least 2 extra candies will have the greatest number of candies among the kids. 
+
+---------------------------------------------------
+array(candies)，個別加上extraCandies的值=最大值(true)，小則(false)
+2+3=5 =>true
+3+3=6 =>true
+5+3=8 =>true
+1+3=4 =>false
+
+*/
+
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function (candies, extraCandies) {
+    // find the max value in the array,can use be use: Math.max(...,candies) which have same result
+    const max = Math.max.apply(null, candies);
+    // to plus extraCnadies into the array
+    const result = candies.map((number => number + extraCandies >= max));
+
+    return result;
+
+};
+const candies = [2, 3, 5, 1, 3]
+const extraCandies = 3;
+console.log(kidsWithCandies(candies, extraCandies));
---
+++ javascript/Array/665.js
@@ -0,0 +1,49 @@
+/*
+Non-decreasing Array
+
+Given an array nums with n integers, your task is to check if it could become non-decreasing by modifying at most 1 element.
+
+We define an array is non-decreasing if nums[i] <= nums[i + 1] holds for every i (0-based) such that (0 <= i <= n - 2).
+
+Constraints:
+
+1 <= n <= 10 ^ 4
+- 10 ^ 5 <= nums[i] <= 10 ^ 5
+
+----------------------------------
+Example 1:
+
+Input: nums = [4,2,3]
+Output: true
+Explanation: You could modify the first 4 to 1 to get a non-decreasing array.
+Example 2:
+
+Input: nums = [4,2,1]
+Output: false
+Explanation: You can't get a non-decreasing array by modify at most one element.
+
+*/
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var checkPossibility = function (nums) {
+    let count = 0;
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] > nums[i + 1]) {
+            if (i === 0 || nums[i + 1] >= nums[i - 1]) {
+                nums[i] = nums[i + 1];
+            } else {
+                nums[i + 1] = nums[i];
+            }
+            count++;
+        }
+
+        if (count > 1) {
+            return false
+        };
+    }
+    return true
+};
+const nums = [4, 2, 3];
+console.log(checkPossibility(nums));
