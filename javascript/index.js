c65b4a9a2e6cf3f7272a06453c989af5918b001a
Mavis <llingchen.27@gmail.com>
2021-01-20 16:29:18 +0800

    edit js

--- javascript/index.js
+++ javascript/index.js
@@ -16,25 +16,51 @@
 // num(10);
 
 
+// 989. Add to Array-Form of Integer
+// /**
+//  * @param {number[]} A
+//  * @param {number} K
+//  * @return {number[]}
+//  */
+// var addToArrayForm = function (A, K) {
+//     if (K <= 0) {
+//         return A;
+//     }
+//     const element = A.join('');
+//     const change = parseInt(element, 10) + parseInt(K, 10);
+//     const result = Array.from(String(change), Number);
+//     // console.log(result);
+//     return result;
+// };
 
+// const A = [1, 2, 6, 3, 0, 7, 1, 7, 1, 9, 7, 5, 6, 6, 4, 4, 0, 0, 6, 3];
+// const K = 516;
+// // should return :[1,2,6,3,0,7,1,7,1,9,7,5,6,6,4,4,0,5,7,9]
+// console.log(addToArrayForm(A, K));
+// // 1200+34 =1234 => [1,2,3,4]
+
+
+// 1512. Number of Good Pairs
 /**
- * @param {number[]} A
- * @param {number} K
- * @return {number[]}
+ * @param {number[]} nums
+ * @return {number}
  */
-var addToArrayForm = function (A, K) {
-    if (K <= 0) {
-        return A;
+var numIdenticalPairs = function (nums) {
+    if (nums.length < 2) {
+        return;
     }
-    const element = A.join('');
-    const change = parseInt(element, 10) + parseInt(K, 10);
-    const result = Array.from(String(change), Number);
-    // console.log(result);
-    return result;
-};
 
-const A = [1, 2, 6, 3, 0, 7, 1, 7, 1, 9, 7, 5, 6, 6, 4, 4, 0, 0, 6, 3];
-const K = 516;
-// [1,2,6,3,0,7,1,7,1,9,7,5,6,6,4,4,0,5,7,9]
-console.log(addToArrayForm(A, K));
-// 1200+34 =1234 => [1,2,3,4]
+
+};
+const nums = [1, 2, 3, 1, 1, 3];
+console.log(numIdenticalPairs(nums));
+/*
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+
+nums[i] == nums[j] and i < j.
+array key i == array key j and key i < key j
+
+key 0 + key 3 => 1 = 1
+*/
