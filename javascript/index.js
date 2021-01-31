f24a8220d9e3c4a5d1c6f2f0f89ace5e1353e93b
Mavis <llingchen.27@gmail.com>
2021-01-31 16:44:08 +0800

    practice linked list js

--- javascript/index.js
+++ javascript/index.js
@@ -8,121 +8,23 @@
 //     alert((end_time - start_time) / 1000 + "sec");
 
 
-
-
-/*
-class MyTest_LinkedList {
-    // default
-    constructor() {
-        this.head = null;
-        this.tail = null;
-    }
-
-    // 新增資料
-    appendData(value) {
-        const newNode = {
-            value: value,
-            next: null
-        }
-
-        if (this.tail) {
-            this.tail = newNode;
-        }
-        this.tail = newNode;
-
-        if (!this.head) {
-            this.head = newNode;
-        }
-    }
-
-    // 在head新增資料
-    appendDataFirst(value) {
-        const newNode = {
-            value: value,
-            next: this.head
-        }
-
-        this.head = newNode;
-        if (!this.tail) {
-            this.tail = newNode;
-        }
-    }
-
-    // 在tail位置新增資料
-    appendDataLast(afterValue, value) {
-        // find the data exist or not
-        const exist = this.findData(afterValue);
-
-        if (exist) {
-            const newNode = {
-                value: value,
-                next: exist.next
-            };
-            exist.next = newNode;
-        }
-    }
-
-    // find this data exist or not
-    findData(value) {
-        // no data at head
-        if (!this.head) {
-            return;
-        }
-        let currentNode = this.head;
-        // console.log(currentNode);
-        while (currentNode) {
-            if (currentNode.value === value) {
-                return currentNode;
-            }
-            currentNode = currentNode.next;
-        }
-        return null;
-    }
-
-    removeData() {
-        this.head = null;
-        this.tail = null;
-    }
-
-    printOut() {
-        const data = [];
-
-        let currentNode = this.head;
-        while (currentNode) {
-            data.push(currentNode);
-            currentNode = currentNode.next;
-        }
-
-        return data;
-    }
-
-}
-// 新增、首位新增、末位新增、找、刪除、printout
-const test = new MyTest_LinkedList();
-test.appendData(20);
-test.appendDataFirst(60);
-console.log(test.printOut());
-*/
-
-
 // 1572. Matrix Diagonal Sum
 /**
  * @param {number[][]} mat
  * @return {number}
  */
-var diagonalSum = function (mat) {
-    for (let index = 0; index <= mat.length; index++) {
-        const element = mat[index];
-        console.log(element);
-
-    }
-};
-const arr = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-];
-console.log(diagonalSum(arr));
+// var diagonalSum = function (mat) {
+//     for (let index = 0; index <= mat.length; index++) {
+//         // const element = mat[index];
+//         console.log(mat[index]);
+//     }
+// };
+// const arr = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+// ];
+// console.log(diagonalSum(arr));
 
 // return the sum of the matrix diagonals. 返回正方形中對角線的總和
 // sum X =>
@@ -148,3 +50,88 @@ Output: 25
 Explanation: Diagonals sum: 1 + 5 + 9 + 3 + 7 = 25
 Notice that element mat[1][1] = 5 is counted only once.
 */
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    appendData(value) {
+        const node = {
+            value: value,
+            next: null
+        };
+
+        if (this.tail) {
+            this.tail = node;
+        }
+
+        this.tail = node;
+
+        if (!this.head) {
+            this.head = node;
+        }
+    }
+
+    appendDataFirst(value) {
+        const node = {
+            value: value,
+            next: this.head
+        }
+        this.head = node;
+        if (!this.tail) {
+            this.tail = node;
+        }
+    }
+
+    appendDataLast(place, value) {
+        const ele = this.find(place);
+
+        if (ele) {
+            const node = {
+                value: value,
+                next: ele.next
+            }
+            ele.next = node;
+        }
+    }
+
+    find(value) {
+        if (!this.head) {
+            return false;
+        }
+
+        let currentNode = this.head;
+
+        while (currentNode) {
+            if (currentNode.next == value) {
+                return currentNode;
+            }
+            currentNode = currentNode.next;
+        }
+        return null;
+    }
+
+    delete() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    printOut() {
+        // empty array
+        const ele = [];
+
+        let currentNode = this.head;
+        while (currentNode) {
+            ele.push(currentNode);
+
+            currentNode = currentNode.next;
+        }
+        return data;
+    }
+}
+const test = new LinkedList();
+test.appendData(10);
+test.appendDataFirst(20);
+console.log(test);
