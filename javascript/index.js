85188a18e92a6fdf5383d7ca271a1734dd547d5e
Mavis <llingchen.27@gmail.com>
2021-02-02 16:26:51 +0800

    add set and que of data structure

--- javascript/index.js
+++ javascript/index.js
@@ -137,119 +137,70 @@ Notice that element mat[1][1] = 5 is counted only once.
 // console.log(test);
 
 /*
-functions:
-add value
-remove value
-find value
-show values
+Queue
+
 */
-class mySet {
-    constructor() {
-        // collection will hold the set as array
-        this.collection = [];
+// class Queue {
+//     constructor() {
+//         this.coll = [];
+//     }
+
+//     print() {
+//         console.log(coll);
+//     }
+
+//     enqueue(ele) {
+//         colln.push(ele);
+//     }
+
+//     deque() {
+//         return coll.shift();
+//     }
+
+//     front() {
+//         return coll[0];
+//     }
+
+//     size() {
+//         return coll.length;
+//     }
+
+//     isEmpty() {
+//         return (coll.length === 0);
+//     }
+// }
+
+function Queue() {
+    // collect all the items of queue
+    let collectionlist = [];
+
+    // print out
+    this.print = function () {
+        console.log(collectionlist);
+    }
+    // add(push) the first item in queue
+    this.enqueue = function (ele) {
+        collectionlist.push(ele);
+    }
+    // take out first item of an queue
+    this.dequeue = function () {
+        return collectionlist.shift();
+    }
+    // return the 1 item of queue
+    this.front = function () {
+        return collectionlist[0];
     }
 
-
-    // add the element to the set
-    addValue(element) {
-        // if the element does't exist
-        if (!this.hasData(element)) {
-            // add the element to the set and return true
-            collection.push(element);
-            return true;
-        }
-        return false;
+    this.size = function () {
+        return collectionlist.length;
     }
-
-    // check for the existence of an element and return true or false
-    hasData(element) {
-        if (collection.indexOf(element) !== -1) {
-            return true;
-        } else {
-            return false;
-        }
-        // return (collection.indexOf(element) !== -1);
-    }
-
-    // remove an element from a set
-    removeValue(element) {
-        if (this.hasData(element)) {
-            let index = collection.indexOf(element);
-            // remove 1 element from index
-            // 從index的位置開始，刪除 1 個元素
-            collection.splice(index, 1);
-            return true;
-        }
-        return false;
-    }
-
-    // return all the values in the set
-    returnValue() {
-        return collection;
-    }
-
-    // return the size of the collection
-    size() {
-        return collection.length;
-    }
-
-    // return the union of 2 sets
-    // which means combine 2 sets
-    combineSets(otherSet) {
-        let unionSet = new Set();
-        let firstSet = this.values();
-        let secondSet = otherSet.values();
-
-        firstSet.forEach(element => {
-            unionSet.addValue(element);
-        });
-
-        secondSet.forEach(element => {
-            unionSet.addValue(element);
-        });
-
-        return unionSet;
-    }
-
-    // return the intersection of 2 sets as a new set
-    intersection(otherSet) {
-        let intersevtionSet = new Set();
-        let firstSet = this.values();
-
-        firstSet.forEach(function (e) {
-            if (otherSet.hasData(e)) {
-                intersevtionSet.addValue(e);
-            }
-        });
-        return intersevtionSet;
-    }
-
-    // return the difference of 2 sets as a new set
-    difference(otherSet) {
-        let differenceSet = new Set();
-        let firstSet = this.values();
-
-        firstSet.forEach(function (e) {
-            if (!otherSet.hasData(e)) {
-                differenceSet.addValue(e);
-            }
-        });
-
-        return differenceSet;
-    }
-
-    // test if the set is a subset of a fifferent set
-    subSet(otherSet) {
-        let firstSet = this.values();
-        return firstSet.every(function (value) {
-            return otherSet.hasData(value);
-        })
+    // check if empty
+    this.isEmpty = function () {
+        return (collectionlist.length === 0);
     }
 }
-
-const setData = new mySet();
-const setData2 = new mySet();
-setData.addValue("a");
-setData2.addValue("b");
-
-console.log(setData.subSet(setData2));
+let q = new Queue();
+q.enqueue('a');
+q.enqueue('asd');
+q.print()
+console.log(q.dequeue());
