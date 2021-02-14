6ce00b154a501f05a115c9d646bc234401683c1e
Mavis <llingchen.27@gmail.com>
2021-02-14 17:17:22 +0800

    edit js

--- javascript/index.js
+++ javascript/index.js
@@ -57,77 +57,16 @@ A.length % 2 == 0
 0 <= A[i] <= 1000
 
 */
-// /**
-//  * @param {number[]} A
-//  * @return {number[]}
-//  */
-// var sortArrayByParityII = function (A) {
-//     if (A.length <= 1) {
-//         return A;
-//     }
-
-// };
-// const array = [4, 2, 5, 7];
-// // should return [4,5,2,7]
-// console.log(`The result is ${sortArrayByParityII(array)}`);
-
-
-/*
-1662. Check If Two String Arrays are Equivalent
-Difficulty:Easy
-
-Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
-
-A string is represented by an array if the array elements concatenated in order forms the string.
-
-1. word1 and word2 = array
-2. if word1 == word2 return true ,else return false
-
-
-Concatenate all strings in the first array into a single string in the given order, the same for the second array.
-Both arrays represent the same string if and only if the generated strings are the same.
-
-Example 1:
-
-Input: word1 = ["ab", "c"], word2 = ["a", "bc"]
-Output: true
-Explanation:
-word1 represents string "ab" + "c" -> "abc"
-word2 represents string "a" + "bc" -> "abc"
-The strings are the same, so return true.
-
-Example 2:
-Input: word1 = ["a", "cb"], word2 = ["ab", "c"]
-Output: false
-
-Example 3:
-Input: word1  = ["abc", "d", "defg"], word2 = ["abcddefg"]
-Output: true
-
-
-Constraints:
-1 <= word1.length, word2.length <= 103
-1 <= word1[i].length, word2[i].length <= 103
-1 <= sum(word1[i].length), sum(word2[i].length) <= 103
-word1[i] and word2[i] consist of lowercase letters.
-
-*/
 /**
- * @param {string[]} word1
- * @param {string[]} word2
- * @return {boolean}
+ * @param {number[]} A
+ * @return {number[]}
  */
-var arrayStringsAreEqual = function (word1, word2) {
-    if (!word1 || !word2) {
-        return;
+var sortArrayByParityII = function (A) {
+    if (A.length <= 1) {
+        return A;
     }
-    if (word1.join("") == word2.join("")) {
-        return true;
-    }
-    return false;
 
 };
-const w1 = ["a", "cb"];
-const w2 = ["ab", "c"];
-// return false
-console.log(`The result is ${arrayStringsAreEqual(w1, w2)}`);
+const array = [4, 2, 5, 7];
+// should return [4,5,2,7]
+console.log(`The result is ${sortArrayByParityII(array)}`);
