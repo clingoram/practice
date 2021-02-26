d769ec23b7719c0cb37fc43f1c85b9932f01e77c
Mavis <llingchen.27@gmail.com>
2021-02-26 20:27:38 +0800

    add 922

--- javascript/index.js
+++ javascript/index.js
@@ -41,20 +41,24 @@ Sort the array so that whenever A[i] is odd, i is odd; and whenever A[i] is even
 
 You may return any answer array that satisfies this condition.
 
-1.array A都是正整數，其中一半是奇數，剩下則是偶數
-2.sort array，array的i是奇數，偶數
+1.array A都是正整數，其中一半是奇數odd，剩下則是偶數even(%2 ===0)
+2.sort array，array的i是奇數也是偶數
 3.return array
+
+
+2 <= nums.length <= 2 * 104
+nums.length is even.
+Half of the integers in nums are even.
+0 <= nums[i] <= 100
 
 Example 1:
 Input: [4,2,5,7]
 Output: [4,5,2,7]
 Explanation: [4,7,2,5], [2,5,4,7], [2,7,4,5] would also have been accepted.
 
-
-Note:
-2 <= A.length <= 20000
-A.length % 2 == 0
-0 <= A[i] <= 1000
+Example 2:
+Input: nums = [2,3]
+Output: [2,3]
 
 */
 /**
@@ -65,6 +69,30 @@ var sortArrayByParityII = function (A) {
     if (A.length <= 1) {
         return A;
     }
+    let result = [];
+
+    // Solution1:
+    // let odd = 1;
+    // let even = 0;
+    // for (let i = 0; i < A.length; i++) {
+    //     if (A[i] % 2 == 0) {
+    //         result[even] = A[i];
+    //         even += 2;
+    //     } else {
+    //         result[odd] = A[i];
+    //         odd += 2;
+    //     }
+    // }
+    // return result;
+
+    // Solution 2:
+    let even = A.filter((i) => i % 2 === 0);
+    let odd = A.filter((i) => i % 2 !== 0);
+
+    for (let index = 0; index < A.length / 2; index++) {
+        result.push(even[index], odd[index]);
+    }
+    return result;
 
 };
 const array = [4, 2, 5, 7];
