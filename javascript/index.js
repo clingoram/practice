66546f4794dc2433e81fce99b6c79c7a6b01eefb
Mavis <llingchen.27@gmail.com>
2021-02-27 15:01:13 +0800

    practice js

--- javascript/index.js
+++ javascript/index.js
@@ -31,75 +31,6 @@ function endTime(start) {
 }
 
 /*
-922. Sort Array By Parity II
-
-Difficulty:Easy
-
-Given an array A of non-negative integers, half of the integers in A are odd, and half of the integers are even.
-
-Sort the array so that whenever A[i] is odd, i is odd; and whenever A[i] is even, i is even.
-
-You may return any answer array that satisfies this condition.
-
-1.array A都是正整數，其中一半是奇數odd，剩下則是偶數even(%2 ===0)
-2.sort array，array的i是奇數也是偶數
-3.return array
-
-
-2 <= nums.length <= 2 * 104
-nums.length is even.
-Half of the integers in nums are even.
-0 <= nums[i] <= 100
-
-Example 1:
-Input: [4,2,5,7]
-Output: [4,5,2,7]
-Explanation: [4,7,2,5], [2,5,4,7], [2,7,4,5] would also have been accepted.
-
-Example 2:
-Input: nums = [2,3]
-Output: [2,3]
-
-*/
-/**
- * @param {number[]} A
- * @return {number[]}
- */
-var sortArrayByParityII = function (A) {
-    if (A.length <= 1) {
-        return A;
-    }
-    let result = [];
-
-    // Solution1:
-    // let odd = 1;
-    // let even = 0;
-    // for (let i = 0; i < A.length; i++) {
-    //     if (A[i] % 2 == 0) {
-    //         result[even] = A[i];
-    //         even += 2;
-    //     } else {
-    //         result[odd] = A[i];
-    //         odd += 2;
-    //     }
-    // }
-    // return result;
-
-    // Solution 2:
-    let even = A.filter((i) => i % 2 === 0);
-    let odd = A.filter((i) => i % 2 !== 0);
-
-    for (let index = 0; index < A.length / 2; index++) {
-        result.push(even[index], odd[index]);
-    }
-    return result;
-
-};
-const array = [4, 2, 5, 7];
-// should return [4,5,2,7]
-console.log(`The result is ${sortArrayByParityII(array)}`);
-
-/*
  105. Construct Binary Tree from Preorder and Inorder Traversal
  Difficulty:Medium
 
@@ -132,25 +63,25 @@ preorder is guaranteed to be the preorder traversal of the tree.
 inorder is guaranteed to be the inorder traversal of the tree.
  */
 
-// /**
-//  * Definition for a binary tree node.
-//  * function TreeNode(val, left, right) {
-//  *     this.val = (val===undefined ? 0 : val)
-//  *     this.left = (left===undefined ? null : left)
-//  *     this.right = (right===undefined ? null : right)
-//  * }
-//  */
-// /**
-//  * @param {number[]} preorder
-//  * @param {number[]} inorder
-//  * @return {TreeNode}
-//  */
-// var buildTree = function (preorder, inorder) {
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function (preorder, inorder) {
 
-// };
+};
 
-// const preorder = [3, 9, 20, 15, 7], inorder = [9, 3, 15, 20, 7];
-// // should return [3,9,20,null,null,15,7]
-// console.log(`The result is ${buildTree(preorder, inorder)}`);
+const preorder = [3, 9, 20, 15, 7], inorder = [9, 3, 15, 20, 7];
+// should return [3,9,20,null,null,15,7]
+console.log(`The result is ${buildTree(preorder, inorder)}`);
 
 
