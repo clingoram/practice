8fc93aa13ee2d208257fd4de0db614cb4a5d6920
Mavis <llingchen.27@gmail.com>
2021-07-14 14:38:42 +0800

    practice to solve 524

--- javascript/index.js
+++ javascript/index.js
@@ -141,3 +141,60 @@ const str = '2 yo man 3 4 2 1';
 // aoy
 // console.log(com(str));
 
+/**
+ * 524. Longest Word in Dictionary through Deleting
+ * Difficulty:Medium
+ * 
+ * Given a string s and a string array dictionary, return the longest string in the dictionary that can be formed by deleting some of the given string characters. 
+ * If there is more than one possible result, return the longest word with the smallest lexicographical order. 
+ * If there is no possible result, return the empty string.
+ * 
+ * 
+ * 
+ * Example 1:
+ * Input: s = "abpcplea", dictionary = ["ale","apple","monkey","plea"]
+ * Output: "apple"
+ * 
+ * Example 2:
+ * Input: s = "abpcplea", dictionary = ["a","b","c"]
+ * Output: "a"
+ * 
+ * Constraints:
+ * 1 <= s.length <= 1000
+ * 1 <= dictionary.length <= 1000
+ * 1 <= dictionary[i].length <= 1000
+ * s and dictionary[i] consist of lowercase English letters.
+ */
+/**
+ * @param {string} s
+ * @param {string[]} dictionary
+ * @return {string}
+ */
+var findLongestWord = function (s, dictionary) {
+	let pattern = /^[a-z]+$/;
+	if (!s.match(pattern) || !s.match(pattern).join("")) {
+		return;
+	}
+
+	// split string s
+	// array.push(), array.pop()
+	let splitS = s.split(''); // obj
+	let splitDict = dictionary.join('').split(''); // obj
+
+	let result = [];
+
+	for (let i = 0; i < splitDict.length; i++) {
+		// console.log(splitDict[i]);
+		// console.log(splitS[i]);
+
+		if (splitDict[i] === splitS[i]) {
+			result.push(splitDict[i]);
+		}
+	}
+	console.log(result);
+	// return result;
+};
+const s = "abpcplea", dictionary = ["ale", "apple", "monkey", "plea"];
+// no 'b','c' in dictionary
+// apple
+// console.log(findLongestWord(s, dictionary));
