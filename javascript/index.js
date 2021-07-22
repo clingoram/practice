d639c03729bc682e53277bc903a83fc9386c582f
Mavis <llingchen.27@gmail.com>
2021-07-22 20:18:39 +0800

    add 53

--- javascript/index.js
+++ javascript/index.js
@@ -35,53 +35,8 @@ function endTime(start) {
 
 	return start[0] + '執行時間:' + (end_time - start[1]) / 5000 + 'ms';
 }
-/*
-53. Maximum Subarray
-Difficulty:Easy
-
-Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
-
-------------------------
-nums = int array
-至少要有一個負數，return 最大總和
-elements中是要連續且有最大正值的elements
-return 最大總和
------------------------
-
-Example 1:
-Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
-Output: 6
-Explanation: [4,-1,2,1] has the largest sum = 6.
-
-Example 2:
-Input: nums = [1]
-Output: 1
-
-Example 3:
-Input: nums = [5,4,-1,7,8]
-Output: 23
 
 
-Constraints:
-1 <= nums.length <= 3 * 104
--105 <= nums[i] <= 105
-*/
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var maxSubArray = function (nums) {
-	if (nums.length < 1) {
-		return null;
-	}
-
-	let max = Math.max(...nums);
-	console.log(max);
-};
-const a = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
-// [4,-1,2,1] has the largest sum = 6 => -1+4+2+1
-// return: 6
-// console.log(maxSubArray(a));
 
 
 /*
@@ -280,11 +235,13 @@ const prices = [7, 1, 5, 3, 6, 4];
 var multiply = function (num1, num2) {
 
 	let pattern = /^[0-9]+gi/;
-	if (!Number(num1).match(pattern) || !Number(num2).match(pattern) || Number(num1) === 0 || Number(num2) === 0) {
+
+	if (!num1.match(pattern) || !num2.match(pattern) || Number(num1) === 0 || Number(num2) === 0) {
 		return;
 	}
+
 
 };
 const num1 = "2", num2 = "3";
 // 6
-console.log(multiply(num1, num2));
+// console.log(multiply(num1, num2));
