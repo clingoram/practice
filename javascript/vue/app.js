8ae906a2cc38d1279434b99ba3014c8b21c8f805
Mavis <llingchen.27@gmail.com>
2021-09-08 08:08:27 +0800

    practice computed and methods

--- javascript/vue/app.js
+++ javascript/vue/app.js
@@ -1,38 +1,25 @@
 let dataM = {
   message: 'Hey Girl.',
-  // html: '<span style="color:red">Turn red</span>',
   copyright: new Date().getFullYear()
 }
 
-// Object.freeze(dataM);
-
-
-// game's data type
-// let game = {
-//   img: 'src',
-//   // img: {
-//   //   src: String,
-//   //   alt: String
-//   // },
-//   title: 'string / html',
-//   description: 'string / html',
-//   price: Number,
-//   price: {
-//     type: String,
-//     count: Number
-//   },
-//   count: Number
-// }
-
 let vm = new Vue({
   el: '#section-for-vue',
-  // template: '<span style="color:red">Turn red</span>',
-  template: '#test',
   data: dataM,
-  creatd: function () {
-    console.log(this);
+  // 資料參數直接處理資料可以用computed
+  computed: {
+    reverseMessageCom: function () {
+      console.log('computed');
+      return this.message.split('').reverse().join('');
+    },
   },
-  method() {
-
-  }
+  // 若computed property和methods同名，會優先較methods，若不同名，則是computed
+  // mwthods要呼叫，每呼叫一次就重新執行
+  methods: {
+    reverseMessage: function () {
+      console.log('method');
+      // 因為是吃dataM的meeage，所以一旦dataM的meeage有改變，所有跟message有關的(不管是computed、method都會跟著變)
+      return this.message.split('').reverse().join('');
+    }
+  },
 })
