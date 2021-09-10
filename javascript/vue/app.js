3b2a51b8e1f768e82c22ffe7f27bfe47f8c8ad39
Mavis <llingchen.27@gmail.com>
2021-09-10 10:12:13 +0800

    practice render(list render and conditional render

--- javascript/vue/app.js
+++ javascript/vue/app.js
@@ -1,42 +1,44 @@
+const mapping = {
+  A: 'A content',
+  B: 'B content',
+  C: 'C content'
+}
 
 let vm = new Vue({
   el: '#section-for-vue',
   data() {
     return {
-      isActive: true,
-      activeClass: 'active',
-      errorClass: 'text-danger',
-      link: {
-        content: 'Google',
-        href: 'http://www.google.com/',
-        target: '_blank',
-        // for style
-        fontColor: 'red'
-      },
-      input: {
-        type: 'text'
-      },
-      // object
-      classObj: {
-        // key:value
-        aaa: true,
-        bbb: false
-      },
-      // 比class obj還差一些，因為這樣他在HTML上會顯示全部陣列，容易出錯。也能用三元運算式(不好)
-      classArray: ['apple', 'orange'],
-      // for style(use obj type)
-      styleObj: {
-        color: 'green',
-        fontSize: '30px'
-      }
+      // 若show=true(顯示)
+      show: false,
+      type: 'A', // defalut
+      // 資料數量多可以外面用陣列包裝，裡面用key:value，包成一組
+      music:
+        [
+          // type obj(key:value) is much better.
+          { singer: 'MAMAMOO', song: 'Traveler', gender: 'F', nation: 'Korea' },
+          { singer: 'MAMAMOO', song: 'Wind Flower', gender: 'F', nation: 'Korea' },
+          { singer: 'Maroon 5', song: 'Beautiful Mistakes', gender: 'M', nation: 'USA' },
+          { singer: 'Jolin', song: '玫瑰少年 Womxnly', gender: 'F', nation: 'Taiwan' },
+          { singer: 'Justin Biber', song: 'Stay', gender: 'M', nation: 'Canada' },
+        ],
+      person:
+        [
+          { name: 'Solar', gender: 'F', age: 30, description: 'Leader of MAMAMOO.' },
+        ],
+      // array.no key:value
+      color: ['red', 'blue', 'orange', 'pink']
     }
   },
+  // 做一次會cache(暫存)下來;methods:每次都會做
   computed: {
-    classComponent: function () {
-      return {
-        active: this.isActive
-      }
+    conditionalContent: function () {
+      return mapping[this.type] || 'No content';
+    },
+    // 在源頭這裡過濾資料
+    filterMusic: function () {
+      return this.music.filter(function (singer) {
+        return singer.gender === 'F';
+      })
     }
   }
-
 })
