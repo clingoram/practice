10bbed8ada85d9e23d8d6fca46df9eed9ec35be5
Mavis <llingchen.27@gmail.com>
2022-10-23 19:09:16 +0800

    新增練習

---
+++ python/codewars/Reversestring.py
@@ -0,0 +1,11 @@
+'''
+Reversed Strings
+
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+'''
+def solution(string):
+  # 反轉字串
+  return string[::-1]
