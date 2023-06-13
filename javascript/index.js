e088c6bfdee59ffe516d1897a86bcf685dc775de
Mavis <llingchen.27@gmail.com>
2023-06-13 11:15:54 +0800

    add one solution

--- javascript/index.js
+++ javascript/index.js
@@ -225,60 +225,7 @@ const target = 6;
 // nums[1] + nums[2] = 6
 // console.log(twoSum(nums, target));
 
-/**
- * Your order, please
- * 
- * Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
- * 
- * Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
- * 
- * If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
- * 
- * Examples
- * "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
- * "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
- * ""  -->  ""
- */
 
-function order(words) {
-	/*
-	input string with number.
-	依據該字串內每個單字所屬的數字做asc排序並回傳正確順序
-	*/
-	let convertToArray = words.split(' ');
-	let numberPattern = /\d+/g;
-	// let result = [];
-	// console.log(words.split(' '));
-
-	for (let i = 0; i < convertToArray.length; i++) {
-		let number = convertToArray[i].match(numberPattern);
-		// convertToArray[i].stroke = convertToArray[i][number];
-		// console.log(number);
-		// let strPattern = convertToArray[i].replace(/[0-9]/g, '');
-		// console.log(strPattern);
-		// console.log(result[number]);
-	}
-
-	let newObjects = convertToArray.map(obj => {
-		return {
-			number: obj.number,
-			value: obj.convertToArray
-		}
-	})
-
-	console.log(newObjects);
-	//   let json = words.split(' ');
-	//   var output = [];
-	//   json.forEach(function (item) {
-	// //         output.push(item.replace(/\'/g, '').split(/(\d+)/).filter(Boolean));
-	//         output.push(item.replace(/\'/g,'').split(/(\d+)/).filter(Boolean));
-
-	//     });
-	//   console.log(output)
-}
-let str = "4of Fo1r pe6ople g3ood th5e the2";
-// "Fo1r the2 g3ood 4of th5e pe6ople"
-// console.log(order(str));
 
 /**
  * Check the exam
