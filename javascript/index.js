a555cd7ea379a8360f27a64b166d96c010c691ed
Mavis <llingchen.27@gmail.com>
2025-03-19 14:41:49 +0800

    try to solve a problem

--- javascript/index.js
+++ javascript/index.js
@@ -324,3 +324,76 @@ var getHappyString = function (n, k) {
 //"cab"
 // console.log(getHappyString(c, k));
 
+
+/**
+ * 2099. Find Subsequence of Length K With the Largest Sum
+ * 
+ * You are given an integer array nums and an integer k. You want to find a subsequence of nums of length k that has the largest sum.
+ * Return any such subsequence as an integer array of length k.
+ * 
+ * A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
+ * 
+ * Hints:
+ * 1.From a greedy perspective, what k elements should you pick?
+ * 2.Could you sort the array while maintaining the index?
+ * 
+ * Example 1:
+ * Input: nums = [2,1,3,3], k = 2
+ * Output: [3,3]
+ * Explanation:
+ * The subsequence has the largest sum of 3 + 3 = 6.
+ * 
+ * Example 2:
+ * Input: nums = [-1,-2,3,4], k = 3
+ * Output: [-1,3,4]
+ * Explanation: 
+ * The subsequence has the largest sum of -1 + 3 + 4 = 6.
+ * 
+ * Example 3:
+ * Input: nums = [3,4,3,3], k = 2
+ * Output: [3,4]
+ * Explanation:
+ * The subsequence has the largest sum of 3 + 4 = 7. 
+ * Another possible subsequence is [4, 3].
+ *  
+ * 
+ * Constraints:
+ * 1 <= nums.length <= 1000
+ * -105 <= nums[i] <= 105
+ * 1 <= k <= nums.length
+ *  
+ * 
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSubsequence = function(nums, k) {
+  // 參數為一個數值陣列nums和數值k，找出符合長度k的最大合subsequence
+  // subsequence = 在不改變元素位置的情況下，移除或保留元素
+  let temp = [];
+  let j = 0;
+  let obj = {};
+  for(let i = 0;i < nums.length;i++){
+    obj[i] = nums[i];
+  }
+
+  while(temp.length !== k){
+    let max = Math.max(...nums);
+    const index = nums.indexOf(max);
+    if (index > -1) { 
+      nums.splice(index, 1);
+      // temp.push(max);
+    }
+  }
+  for(const [key,value] of Object.entries(obj)){
+    console.log(key)
+  }
+  // console.log(temp);
+};
+let nums = [-1,-2,3,4], k = 3;
+// [-1,3,4]
+// let nums = [3,4,3,3], k = 2;
+// [4,3]
+// let nums = [2,1,3,3], k = 2;
+// 「3,3]
+console.log(maxSubsequence(nums,k))
